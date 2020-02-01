3834194e415ce74727c42574d809693ef614357f
Jack S M Zheng <549632525@qq.com>
2020-02-01 11:38:17 +0800

    Update health page

--- huatuo/pages/submithealth/submithealth.js
+++ huatuo/pages/submithealth/submithealth.js
@@ -26,61 +26,125 @@ Page({
       isMandatory: true,
       isCRSRelated: false,
       maxlength: 8,
-      label: 'Staf ID (员工号)',
-      confirmLabel: 'Staf ID (员工号)',
+      label: '1. 你的员工编号 Your Staff ID:',
+      confirmLabel: '1. 你的员工编号 Your Staff ID:',
       bindInputName: 'inputEvent',
       warningLabel: 'Please Enter Staf ID (请输入员工号)',
+      num: '1',
       content: ''
     },
-    visitHuBei: {
+    mobile: {
+      hasLabel: true,
+      hasWarning: false,
+      isMandatory: true,
+      isCRSRelated: false,
+      maxlength: 8,
+      label: '2. 你的紧急联系电话 Your cell phone for emergency call:',
+      confirmLabel: '2. 你的紧急联系电话 Your cell phone for emergency call:',
+      bindInputName: 'inputEvent',
+      warningLabel: 'Please Enter cell phone (请输入紧急联系电话)',
+      num: '2',
+      content: ''
+    },
+    others: {
       items: [{
         id: 1,
-        name: '去过湖北',
+        name: '是 Yes',
       }, {
         id: 2,
-        name: '接触过来自湖北的客人/亲友'
-      }, {
-        id: 3,
-        name: '接触过有以下症状的病人(发热38度以上、咳嗽、咳痰等)'
-      }, {
-        id: 4,
-        name: '以上情况均无',
+        name: '不是 No'
       }],
-      title: '自2020年1月1日以来是否有一下情况:(*)',
+      title: '3. 你为其他同事报告吗 Are you reporting for other colleague? *',
       current: '-',
       position: 'left',
       checked: false,
       disabled: false,
     },
-    healths: {
+    status: {
       items: [{
         id: 1,
-        name: '发热',
+        name: '由医疗机构或者疾控部门告知的确诊病毒性肺炎案例 Confirmed viral pneumonia as advised by hospital/authority',
       }, {
         id: 2,
-        name: '干咳'
+        name: '由医疗机构或者疾控部门告知的疑似病毒性肺炎案例 Suspected viral pneumonia as advised by hospital/authority'
       }, {
         id: 3,
-        name: '乏力'
+        name: '由医疗机构或者疾控部门告知的密切接触 Close contact as advised by hospital/authority'
       }, {
         id: 4,
-        name: '腹泻',
+        name: '有发烧但不是肺炎 Fever but not pneumonia',
       }, {
-      id: 5,
-      name: '感冒',
+        id: 5,
+        name: '没有发烧，但有其它身体不适的情况（请提供具体的症状）Not fever but feel uncomfortable (please provide the detail of symptoms)'
       }, {
         id: 6,
-        name: '头疼头晕',
+        name: '居住楼或小区被有关部门限制出入 The building or residential estate lived in is being restricted.'
+        }],
+      title: '4. 你或者被你报告的同事，现在的情况是 What is the current circumstance of the reported colleague? *',
+      current: '-',
+      position: 'left',
+      checked: false,
+      disabled: false,
+    },
+    address: {
+      hasLabel: true,
+      hasWarning: false,
+      isMandatory: true,
+      isCRSRelated: false,
+      maxlength: 8,
+      label: '5. 你或者被你报告的同事，现在的所在地 Where are you or the reported colleague located?',
+      confirmLabel: '5. 你或者被你报告的同事，现在的所在地 Where are you or the reported colleague located?',
+      bindInputName: 'inputEvent',
+      warningLabel: 'Please Enter your locations (请输入所在地)',
+      num: '3',
+      content: ''
+    },
+    visits: {
+      items: [{
+        id: 1,
+        name: '无 None',
       }, {
-        id: 7,
-        name: '以上均没有',
+        id: 2,
+        name: '广州 GZ'
+      }, {
+        id: 3,
+        name: '西安 XA'
       }],
-      title: '您是否有以下症状:(*)',
+      title: '6. 14天之内去过的办公地点 Which office did you visit in 14 days? *',
       current: [],
       position: 'left',
       checked: false,
       disabled: false,
-    }
+    },
+    supports: {
+      items: [{
+        id: 1,
+        name: '需要提供紧急支持 (请提供联系方式及需要支持的内容） Yes, need the emergency support (please provide the contact number and what kind of support is required)',
+      }, {
+        id: 2,
+        name: '不需要提供紧急支持 (No need for emergency support)'
+      }],
+      title: '7. 你或者被你报告的同事，需要公司提供紧急支持吗 Does the reported colleague need emergency support from the company?',
+      current: '-',
+      position: 'left',
+      checked: false,
+      disabled: false,
+    },
+    remote: {
+      items: [{
+        id: 1,
+        name: '可以远程工作 Yes, can work from home',
+      }, {
+        id: 2,
+        name: '无法远程工作(请提供具体原因）No, cannot work from home (please describe your current status)'
+      }],
+      title: '8. 你或者被你报告的同事，如身体情况允许远程工作，是否有远程工作的条件，包括Laptop，V.P.N token，Internet WIFI等 If the health condition allows you or the reported colleague to work remotely, do you or the reported colleague have sufficient facilities to work from home, including Laptop, V.P.N token, and internet WIFI, etc?',
+      current: '-',
+      position: 'left',
+      checked: false,
+      disabled: false,
+    },
+    date: ''
   },
 
   /**
@@ -162,24 +226,61 @@ Page({
   inputEvent: function (e_) {
     const e = e_.detail.e ? e_.detail.e : e_
     console.log('input event : ', e)
-    
+    var num = e.currentTarget.dataset.num;
+    var field;
+    switch(num) {
+      case '1': field = 'stafID.content'; break;
+      case '2': field = 'mobile.content'; break;
+    }
     this.setData({
-      ['stafID.content']: e.detail.value
+      [field]: e.detail.value
     })
   },
   //
-  handleVisitHubeiChange({ detail = {} }) {
+  handleOthersChange({ detail = {} }) {
     console.log(detail.value)
     this.setData({
-      ['visitHuBei.current']: detail.value
+      ['others.current']: detail.value
     });
   },
   //
-  handleHealthChange({ detail = {} }) {
-    const index = this.data.healths.current.indexOf(detail.value);
-    index === -1 ? this.data.healths.current.push(detail.value) : this.data.healths.current.splice(index, 1);
+  handleStatusChange({ detail = {} }) {
+    console.log(detail.value)
     this.setData({
-      ['healths.current']: this.data.healths.current
+      ['status.current']: detail.value
     });
   },
+  //
+  handleSupportsChange({ detail = {} }) {
+    console.log(detail.value)
+    this.setData({
+      ['supports.current']: detail.value
+    });
+  },
+  //
+  handleRemoteChange({ detail = {} }) {
+    console.log(detail.value)
+    this.setData({
+      ['remote.current']: detail.value
+    });
+  },
+  //
+  handleVisitsChange({ detail = {} }) {
+    const index = this.data.visits.current.indexOf(detail.value);
+    index === -1 ? this.data.visits.current.push(detail.value) : this.data.visits.current.splice(index, 1);
+    this.setData({
+      ['visits.current']: this.data.visits.current
+    });
+  },
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+
+  submitHealthForm(e) {
+    console.log(e.detail.value);
+  }
 })
